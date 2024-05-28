ad12680824deea2bf7ec78742bf76ee87af8f648
SJKIM\ksj10 <ksj10111011@naver.com>
2024-05-28 17:20:17 +0900

    feat : init project luis test중.. 유료인가?

---
+++ 0_project/WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,114 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Azure.CognitiveServices.Language.LUIS.Runtime;
+using Microsoft.Azure.CognitiveServices.Language.LUIS.Runtime.Models;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static readonly string endpoint = "https://<your-luis-endpoint>.api.cognitive.microsoft.com";
+        private static readonly string subscriptionKey = "<your-luis-subscription-key>";
+        private static readonly string appId = "<your-luis-app-id>";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void ProcessButton_Click(object sender, RoutedEventArgs e)
+        {
+            string userCommand = CommandTextBox.Text;
+            var predictionResult = await GetLuisPrediction(userCommand);
+            var parameters = ExtractParameters(predictionResult);
+            ExecuteCalibration(parameters);
+        }
+
+        private async Task<PredictionResponse> GetLuisPrediction(string query)
+        {
+            var client = new LUISRuntimeClient(new ApiKeyServiceClientCredentials(subscriptionKey))
+            {
+                Endpoint = endpoint
+            };
+
+            var requestOptions = new PredictionRequest
+            {
+                Query = query
+            };
+
+            var predictionResult = await client.Prediction.GetSlotPredictionAsync(appId, "Production", requestOptions);
+            return predictionResult;
+        }
+
+        private CalibrationParameters ExtractParameters(PredictionResponse predictionResult)
+        {
+            var parameters = new CalibrationParameters();
+
+            foreach (var entity in predictionResult.Prediction.Entities)
+            {
+                switch (entity.Key)
+                {
+                    case "startFrequency":
+                        parameters.StartFrequency = Convert.ToDouble(entity.Value[0]);
+                        break;
+                    case "endFrequency":
+                        parameters.EndFrequency = Convert.ToDouble(entity.Value[0]);
+                        break;
+                    case "stepSize":
+                        parameters.StepSize = Convert.ToDouble(entity.Value[0]);
+                        break;
+                    case "inputOffset":
+                        parameters.InputOffset = Convert.ToDouble(entity.Value[0]);
+                        break;
+                    case "outputOffset":
+                        parameters.OutputOffset = Convert.ToDouble(entity.Value[0]);
+                        break;
+                }
+            }
+
+            return parameters;
+        }
+
+        private void ExecuteCalibration(CalibrationParameters parameters)
+        {
+            ResultTextBlock.Text = $"Calibrating from {parameters.StartFrequency} Hz to {parameters.EndFrequency} Hz with step size {parameters.StepSize} Hz\n";
+            ResultTextBlock.Text += $"Input offset: {parameters.InputOffset} dB, Output offset: {parameters.OutputOffset} dB\n";
+
+            // Here you would add the code to control your hardware.
+            // For example, sending commands to an RF signal generator, spectrum analyzer, etc.
+
+            // Example pseudo-code:
+            // Initialize hardware connections
+            // for (double freq = parameters.StartFrequency; freq <= parameters.EndFrequency; freq += parameters.StepSize)
+            // {
+            //     SetSignalGeneratorFrequency(freq);
+            //     SetInputOffset(parameters.InputOffset);
+            //     MeasureOutput(parameters.OutputOffset);
+            //     RecordResults();
+            // }
+            // Finalize and disconnect
+
+            ResultTextBlock.Text += "Calibration complete.";
+        }
+
+        public class CalibrationParameters
+        {
+            public double StartFrequency { get; set; }
+            public double EndFrequency { get; set; }
+            public double StepSize { get; set; }
+            public double InputOffset { get; set; }
+            public double OutputOffset { get; set; }
+        }
+    }
+}
